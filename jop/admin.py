d5a34e5f2c3179b7e1b8909f369b0c842ff5c427
Omarmoatz <omar.raouf.002@gmail.com>
2023-10-24 03:59:54 +0300

    design jop models and added it to admin

--- jop/admin.py
+++ jop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Jop, Company ,Category
+
+
+admin.site.register(Jop)
+admin.site.register(Company)
+admin.site.register(Category)
+
 
 # Register your models here.
--- jop/models.py
+++ jop/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django_countries.fields import CountryField
+from django.utils import timezone
 
-# Create your models here.
+# Create your models here.\
+
+JOP_CHOISE = (
+    ('FullTime','Full_Time'),
+    ('PartTime','PartTime'),
+    ('Remote','Remote'),
+    ('Freelance','Freelance')
+)
+
+class Jop(models.Model):
+    title = models.CharField(max_length=150)
+    location = CountryField()
+    company =models.ForeignKey('Company', on_delete=models.CASCADE, related_name='jop_company')
+    salary_start = models.PositiveIntegerField(null=True, blank=True)
+    salary_end = models.PositiveIntegerField(null=True, blank=True)
+    cratesd_at = models.DateTimeField(default=timezone.now)
+    vacancy = models.PositiveIntegerField()
+    experince = models.PositiveIntegerField()
+    jop_nature = models.CharField(choices=JOP_CHOISE , max_length=10)
+    description = models.TextField(max_length=5000)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL,null=True, blank=True, related_name='jop_category')
+    
+    def __str__(self) :
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    logo = models.CharField(max_length=50)
+
+    def __str__(self) :
+        return self.name
+
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=150)
+    logo = models.ImageField(upload_to='company')
+    website = models.URLField()
+    email = models.EmailField()
+    description = models.TextField(max_length=5000)
+
+    def __str__(self) :
+        return self.name
