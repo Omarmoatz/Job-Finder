ed086b39f2fbd01670b7766b49d66b408f1866f6
Omarmoatz <omar.raouf.002@gmail.com>
2023-10-25 20:57:09 +0300

    work with dates

--- project/settings.py
+++ project/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     #3rd apps
 
     #my apps
-    'jop'
+    'jop',
 ]
 
 MIDDLEWARE = [
