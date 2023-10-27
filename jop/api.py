4da58452793fad6e40249be7f032935a37c0b978
Omarmoatz <omar.raouf.002@gmail.com>
2023-10-27 04:00:17 +0200

    add update and delete by class based viwe(genaric)

--- jop/api.py
+++ jop/api.py
@@ -1,19 +1,28 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
 
 from .models import Jop
 from .serializers import JopSerializer
 
-
+'''
 @api_view(['GET'])
 def job_list_api(request):
     job = Jop.objects.all()
     data = JopSerializer(job, many=True).data
     return Response({'job':data})
 
-
 @api_view(['GET'])
 def job_detail_api(request,id):
     job =Jop.objects.get(id=id)
     data = JopSerializer(job).data
     return Response({'job':data})
+'''
+
+class JobListApi(generics.ListCreateAPIView):
+    queryset = Jop.objects.all()
+    serializer_class = JopSerializer
+
+class JobDetailtApi(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Jop.objects.all()
+    serializer_class = JopSerializer
