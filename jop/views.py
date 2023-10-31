f0ddebcfe4e6908bfd3c86e3c6593c272977fad6
Omarmoatz <omar.raouf.002@gmail.com>
2023-10-31 19:13:54 +0200

    added pagination from (vitor)

--- jop/views.py
+++ jop/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.views import generic
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Jop , JopForm
 
 def jop_list(request):
     all_jobs = Jop.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(all_jobs, 40)
+    try:
+        all_jobs = paginator.page(page)
+    except PageNotAnInteger:
+        all_jobs = paginator.page(1)
+    except EmptyPage:
+        all_jobs = paginator.page(paginator.num_pages)
     return render(request, 'jop/job_list.html', {'jop':all_jobs})
+
 
 def jop_detail(request, slug):
     jobs = Jop.objects.get(slug=slug)
