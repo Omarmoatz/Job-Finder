1bf9467f3081d8944381a8a786accaecb3047875
Omarmoatz <omar.raouf.002@gmail.com>
2024-01-05 15:38:59 +0200

    fix some problems

--- jop/views.py
+++ jop/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.cache import cache_page
 from .models import Job , JobForm
 from .forms import AddForm
