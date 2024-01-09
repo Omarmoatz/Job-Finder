abd191c659967f623b32f2eb6edf601da5a80859
Omarmoatz <omar.raouf.002@gmail.com>
2024-01-09 19:38:23 +0200

    add blog to home page

--- settings/views.py
+++ settings/views.py
@@ -1,13 +1,16 @@
-from jop.models import Job,Category,Company
-
 from django.shortcuts import render
 from django.db.models import Count
+
+from jop.models import Job,Category,Company
+from blog.models import Blog
 
 
 def home(request):
     category = Category.objects.all()[:8].annotate(job_num=Count('jop_category'))
     job = Job.objects.all()[:5]
+    blog = Blog.objects.all()[:2]
     return render(request, 'settings/home.html',{
         'ctg':category,
         'job':job,
+        'blog':blog,
     })
