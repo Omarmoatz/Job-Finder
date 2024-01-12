b35041e2e2b9fd6ee78a3e3dfcbd8b4641bc27f7
Omarmoatz <omar.raouf.002@gmail.com>
2024-01-12 03:41:49 +0200

    add profile signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from utiles.generate_code import genrate_code
 
@@ -14,6 +15,12 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user)
 
+@receiver(post_save,sender=User)
+def profile_signal(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(
+            user = instance ,
+        )
 
 
 
