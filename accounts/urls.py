dc5a3c95f4c331ec66b044eac3d82d9e08ac478e
Omarmoatz <omar.raouf.002@gmail.com>
2024-01-12 22:18:58 +0200

    add activation page

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', signUp, name='signup'),
-    path('signup/<str:username>/activat', activate, name='signup_activate'),
+    path('signup/<str:username>/activate', activate, name='signup_activate'),
     path('profile/', profile, name='profile'),
     path('profile/edit', edit_profile, name='profile_edit'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import Profile
+from .models import Profile,User
 from .forms import ProfileForm,UserForm,SignUpForm,ActivationForm
 
 from django.core.mail import send_mail
@@ -20,18 +20,37 @@ def signUp(request):
             profile = Profile.objects.get(user__username=username)
             send_mail(
                 "Activation Code",
-                f"Welcome {username} Use This Code :{profile.code}",
+                f"Welcome {username} Use This Code :{profile.code} To Activate Your Account",
                 "JopFinder@gmail.com",
                 [email],
                 fail_silently=False,
             )
-            return redirect(f'/registration/{username}/activate')
+            return redirect(f'/accounts/signup/{username}/activate')
     else:
         signupForm = SignUpForm()
     return render(request,'registration/signup.html',{'form':signupForm})
 
-def activate(request):
-    return
+def activate(request,username):
+    profile = Profile.objects.get(user__username=username)
+    if request.method == 'POST':
+        activationForm = ActivationForm(request.POST)
+        if activationForm.is_valid():
+            code = activationForm.cleaned_data['code']
+            if code == profile.code:
+
+                # to make the user login to page after useing code
+                user = User.objects.get(username=profile.user.username)
+                user.is_active = True
+                user.save()
+
+                profile.code = ''
+                profile.save()
+                return redirect('/accounts/login/')
+    else:
+        activationForm = ActivationForm()
+
+    return render(request,'registration/activation.html',{'form':activationForm})
+
 
 
 def profile(request):
