46b705c74340cf57b31bb092a7e59dd92572748d
Omarmoatz <omar.raouf.002@gmail.com>
2024-01-13 14:48:56 +0200

    design contact models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class MyData(models.Model):
+    address = models.CharField(max_length=200)
+    phone_num = models.PositiveIntegerField()
+    email = models.EmailField( max_length=300)
+
+    def __str__(self):
+        return self.email
