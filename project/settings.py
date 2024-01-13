43ce0e286827e973ec6a3315019c1d8fb2e2d64e
Omarmoatz <omar.raouf.002@gmail.com>
2024-01-13 16:13:17 +0200

    add send message to gemail from contact

--- project/settings.py
+++ project/settings.py
@@ -205,4 +205,12 @@ if DEBUG:
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'omar.raouf.002@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
--- settings/views.py
+++ settings/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.db.models import Count
+from django.core.mail import send_mail
 
 from .models import Main
 from jop.models import Job,Category,Company
 from blog.models import Blog
+from django.conf import settings
 
 
 def home(request):
@@ -18,6 +20,19 @@ def home(request):
 
 def contact(request):
     contact = Main.objects.last()
+
+    if request.method == 'POSt':
+        subject = request.POST['subject']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        send_mail(
+            subject,
+            message,
+            email,
+            [settings.EMAIL_HOST_USER],
+        )
+
     return render(request,'settings/contact.html',{
         'contact':contact,
     })
